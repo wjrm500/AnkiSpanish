c8fc4c53c12398f1068dea6187f3110d1851a9d9
William May <wjrm500@gmail.com>
2023-11-30 12:18:46 +0000

    Further work

--- main.py
+++ main.py
@@ -1,62 +1,92 @@
+import copy
+import os
 import logging
+from typing import List
 
 from anki.storage import Collection
+from anki.notes import Note
 
-# Setup logging
-logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
+# Set up logging
+logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-# Path to Anki collection
-collection_path = "C:\\Users\\wjrm5\\AppData\\Roaming\\Anki2\\User 1\\collection.anki2"
+class ReadableFields:
+    rank: str
+    word: str
+    part_of_speech: str
+    definition: str
+    spanish: str
+    english: str
+    freq: str
 
-# Function to modify cards (empty for now)
-def modify_card(card):
-    pass
+    def __init__(self, fields: List) -> None:
+        self.rank = fields[0]
+        self.word = fields[1]
+        self.part_of_speech = fields[2]
+        self.definition = fields[3]
+        self.spanish = fields[4]
+        self.english = fields[5]
+        self.freq = fields[6]
+    
+    def retrieve(self) -> List:
+        return [
+            self.rank,
+            self.word,
+            self.part_of_speech,
+            self.definition,
+            self.spanish,
+            self.english,
+            self.freq
+        ]
 
-try:
-    # Open the collection
-    collection = Collection(collection_path, log=True)
-    logger.info("Opened the collection.")
+def modify_note(note: Note) -> Note:
+    return note
 
-    # Check for the existence of the decks
+def main():
+    collection_path = "C:\\Users\\wjrm5\\AppData\\Roaming\\Anki2\\User 1\\collection.anki2"
+    
+    if not os.path.exists(collection_path):
+        logger.error(f"Collection not found at {collection_path}")
+        return
+
+    # Load the collection
+    logger.info(f"Loading collection from {collection_path}")
+    col = Collection(collection_path)
+
+    # Check for deck existence and create if necessary
     original_deck_name = "A Frequency Dictionary of Spanish"
-    edited_deck_name = "A Frequency Dictionary of Spanish (Edited)"
+    new_deck_name = "A Frequency Dictionary of Spanish (Edited)"
 
-    original_deck_id = collection.decks.id(original_deck_name, create=False)
-    edited_deck_id = collection.decks.id(edited_deck_name, create=False)
+    if original_deck_name in col.decks.all_names():
+        if new_deck_name not in col.decks.all_names():
+            logger.info(f"Creating new deck '{new_deck_name}'")
+            col.decks.id(new_deck_name)  # This creates a new deck
 
-    if edited_deck_id:
-        logger.info(f"Deck '{edited_deck_name}' already exists.")
+        # Process cards in the original deck
+        original_deck_id = col.decks.id(original_deck_name)
+        new_deck_id = col.decks.id(new_deck_name)
+        card_ids = col.decks.cids(original_deck_id)
+
+        logger.info(f"Processing {len(card_ids)} cards from '{original_deck_name}'")
+        model = col.models.by_name("A Frequency Dictionary of Spanish")
+        for cid in card_ids:
+            original_card = col.get_card(cid)
+            original_note = original_card.note()
+            new_note = col.new_note(model)
+            readable_fields = ReadableFields(original_note.fields)
+            readable_fields.definition = "Test"
+            new_note.fields = readable_fields.retrieve()
+            col.add_note(note=new_note, deck_id=new_deck_id)
+
     else:
-        if original_deck_id:
-            logger.info(f"Deck '{original_deck_name}' found. Creating a copy named '{edited_deck_name}'.")
-            # Copy the deck
-            collection.decks.copy(original_deck_id, edited_deck_name)
-            edited_deck_id = collection.decks.id(edited_deck_name)
-            # Copy cards from the original deck to the new deck
-            card_ids = collection.find_cards(f"'deck:{original_deck_id}'")
-            for card_id in card_ids:
-                card = collection.get_card(card_id)
-                card.did = edited_deck_id
-                card.flush()
-        else:
-            logger.error(f"Deck '{original_deck_name}' not found. Exiting script.")
-            raise Exception(f"Deck '{original_deck_name}' not found.")
+        logger.error(f"Deck '{original_deck_name}' not found")
+        col.close()
+        return
 
-    # Iterate over the cards in the copied deck
-    for cid in collection.find_cards(f"'deck:{edited_deck_name}'"):
-        card = collection.get_card(cid)
-        modify_card(card)  # Call the modify_card function on each card
+    # Save changes and close the collection
+    col.save()
+    col.close()
+    logger.info("Processing complete")
 
-    # Save the changes and close the collection
-    collection.save()
-    collection.close()
-    logger.info("Changes saved and collection closed.")
-
-except Exception as e:
-    logger.error(f"An error occurred: {e}")
-
-finally:
-    if "collection" in locals():
-        collection.close()
-        logger.info("Collection closed.")
+if __name__ == "__main__":
+    main()
