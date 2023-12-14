13fd79003fc40b2e9a184fa70ab426c07567bbcb
William May <wjrm500@gmail.com>
2023-12-14 16:23:51 +0000

    Tests for synonym module

---
+++ test/test_synonym.py
@@ -0,0 +1,22 @@
+from synonym import SynonymChecker
+
+
+def test_get_synonyms():
+    synonyms = SynonymChecker.get_synonyms("huge", pos="a")
+    assert "immense" in synonyms
+    assert "vast" in synonyms
+
+    synonyms = SynonymChecker.get_synonyms("qwerty", pos="n")
+    assert len(synonyms) == 0
+
+
+def test_are_synonymous():
+    assert SynonymChecker.are_synonymous("huge", "immense", pos="a")
+    assert not SynonymChecker.are_synonymous("happy", "sad", pos="a")
+
+
+def test_mark_synonymous_words():
+    words = ["huge", "immense", "vast", "small", "little"]
+    marks = SynonymChecker.mark_synonymous_words(words, pos="a")
+    expected_marks = [0, 1, 1, 0, 1]  # 1 means synonymous with an earlier word
+    assert marks == expected_marks
