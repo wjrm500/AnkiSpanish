41ef6103dea7a682ab354193124940030e7b130b
William May <wjrm500@gmail.com>
2023-12-19 15:18:39 +0000

    Modifying setup.py to read in requirements

--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
 from setuptools import setup, find_packages
 
+def read_requirements(file):
+    with open(file) as f:
+        return f.read().splitlines()
+
 setup(
     name="lexideck",
-    version="1.0.0",
+    use_scm_version=True,
+    setup_requires=["setuptools_scm"],
     packages=find_packages(),
+    install_requires=read_requirements("requirements.txt"),
+    extras_require={
+        "dev": read_requirements("requirements-dev.txt"),
+    },
     entry_points={
         "console_scripts": [
             "lexideck = app.main:main",
